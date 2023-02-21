2805c681bd523bde4709eb370e4491499a0b9d85
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-02-21 09:03:13 +0545

    Completed Guessing Game Phase 2

---
+++ guessing-game.js
@@ -0,0 +1,85 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+
+     input: process.stdin,
+    output: process.stdout
+
+});
+
+
+let secretNumber;
+
+let askGuess = function () {
+
+    rl.question (" Enter your guess! ", answer => {
+
+        
+        if(checkNum(Number(answer))){
+            
+            console.log("You Win!")
+            rl.close();
+
+        }else{
+            
+            askGuess();
+
+        }
+    });
+   
+    
+ 
+ 
+ }
+ 
+let checkNum=function(num) {
+
+    if ( num > secretNumber ) {
+
+    console.log( "Too high!" );
+    return false;
+
+    } else if ( num < secretNumber ) {
+
+    console.log( "Too low!" );
+    return false;
+
+    } else if( num === secretNumber ){
+
+    console.log( "Correct!" ) ;
+    return true;
+
+    }else {
+
+    console.log("Make sure to Enter a number !!")
+    return false;
+    
+    }
+}
+
+let randomInrange = function( min , max ) {
+    
+        min = Math.ceil( min );
+
+        max = Math.floor( max );
+
+        return Math.floor ( Math.random () * ( max - min + 1 ) + min );
+      
+}
+let askRange=function () {
+
+    rl.question( "Enter a max Number. " , max =>
+        {
+             rl.question("Enter a min Number. ", min => 
+            {
+
+                secretNumber = randomInrange( min , max );
+
+                askGuess();
+            
+            })
+        }
+    );
+}
+    askRange();
+    
